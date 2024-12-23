84b74423ecc95ad949f075238b0ca1bba9c45cdd
Pingis2 <anton@worksit.se>
2024-12-23 09:38:30 +0100

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
 
 app.use(
     cors({
-        origin: "http://localhost:5173", // Replace with your frontend origin during development
+        origin: "https://express-test-pearl.vercel.app/api/", // Replace with your frontend origin during development
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
