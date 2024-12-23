11f169e1a3cb9b46740be9c452849818721a1d86
Pingis2 <anton@worksit.se>
2024-12-23 09:36:41 +0100

    added cors

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -42,6 +43,17 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(
+    cors({
+        origin: "http://localhost:5173", // Replace with your frontend origin during development
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
+app.options("*", cors());
 
 app.use((req, res, next) => {
     if (!req.app.locals.db) {
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
+    const navigate = useNavigate();
+
+    const button = () => {
+        navigate("/login");
+    }
+
     return (
-        <>Home</>
+        <>
+            Home
+            <button onClick={button}>Login</button>
+        </>
     );
 }
 
