98f94a09af13f4ac0bc950c44337e22b86413498
Pingis2 <anton@worksit.se>
2024-12-25 09:38:20 +0100

    added logs

--- frontend/src/services/serviceBase.ts
+++ frontend/src/services/serviceBase.ts
@@ -2,10 +2,18 @@ import axios from "axios";
 
 export const get = async <T>(url: string) => {
     try {
+        console.log("api url", url)
         const response = await axios.get<T>(url);
+        console.log("api response", response);
+        
+        
         return response;
     } catch (error) {
-        console.error("Error during API call:", error);
+        if (axios.isAxiosError(error)) {
+            console.error("Axios error:", error.response?.data || error.message);
+        } else {
+            console.error("Unexpected error:", error);
+        }
         throw error;
     }
 };
