1fc05110d3484c228d941448f3b956f82d59239d
Pingis2 <anton@worksit.se>
2024-12-25 12:11:03 +0100

    updated getUsers

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,9 +1,9 @@
 import { IUserData, IUsers } from "../models/IUsers";
-import { /*get*/ post } from "./serviceBase";
+import { get, post } from "./serviceBase";
 
 const BASE_URL = 'https://express-test-pearl.vercel.app/api/';
 
-/*
+
 export const getUsers = async (): Promise<IUserData> => {
     try {
         const response = await get<IUserData>(
@@ -13,17 +13,17 @@ export const getUsers = async (): Promise<IUserData> => {
         console.log("api response", response);
 
         if (response.data && response.data.users) {
-            return { users: response.data.users };
+            return { users: response.data.users, token: response.data.token };
         } else {
             console.error("No users found in the response");
-            return { error: true };
+            return { error: true, token: '' };
         }
     } catch (error) {
         console.error("Error during API call:", error);
         throw error;
     }
 };
-*/
+
 
 export const loginUser = async (email: string, password: string): Promise<IUserData | null> => {
     try {
