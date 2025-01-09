48491468660dcdeaea42afe6a84b2acd958cb962
Pingis2 <anton@worksit.se>
2025-01-09 16:21:41 +0100

    added jungle veins

--- frontend/src/pages/Start.tsx
+++ frontend/src/pages/Start.tsx
@@ -3,6 +3,10 @@ import LogoutButton from "../components/Logout";
 import { UserContext } from "../context/UserContext";
 import { useContext, useEffect } from "react";
 import { CountryFlag } from "../components/CountryFlag";
+import Jungle1 from "../assets/images/jungle/jungle-1.png";
+import Jungle2 from '../assets/images/jungle/jungle-2.png';
+import Jungle3 from '../assets/images/jungle/jungle-3.png';
+import Jungle4 from '../assets/images/jungle/jungle-4.png';
 
 export const StartPage = () => {
     const { user, loading } = useContext(UserContext);
@@ -29,6 +33,10 @@ export const StartPage = () => {
                     <p className="user-info">{user?.userName} {user && <CountryFlag user={user} />}</p>
                     <LogoutButton />
                 </div>
+                <img src={Jungle1} alt="Jungle" className="jungle-1" />
+                <img src={Jungle2} alt="Jungle" className="jungle-2" />
+                <img src={Jungle3} alt="Jungle" className="jungle-3" />
+                <img src={Jungle4} alt="Jungle" className="jungle-4" />
             </header>
             
             <section>
