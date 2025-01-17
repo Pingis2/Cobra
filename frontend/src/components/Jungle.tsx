112d8203c84a400529b678f39bc24d5427710fb8
Pingis2 <anton@worksit.se>
2025-01-17 12:39:10 +0100

    updated images to webp

--- frontend/src/components/Jungle.tsx
+++ frontend/src/components/Jungle.tsx
@@ -1,9 +1,18 @@
-import Jungle1 from "../assets/images/jungle/jungle-1.png";
-import Jungle2 from '../assets/images/jungle/jungle-2.png';
-import Jungle3 from '../assets/images/jungle/jungle-3.png';
-import Jungle4 from '../assets/images/jungle/jungle-4.png';
+import { useEffect } from "react";
+import Jungle1 from "../assets/images/jungle/jungle-1.webp";
+import Jungle2 from '../assets/images/jungle/jungle-2.webp';
+import Jungle3 from '../assets/images/jungle/jungle-3.webp';
+import Jungle4 from '../assets/images/jungle/jungle-4.webp';
 
 export const Jungle = () => {
+    useEffect(() => {
+        const images = [Jungle1, Jungle2, Jungle3, Jungle4];
+        images.forEach((src) => {
+            const img = new Image();
+            img.src = src;
+        });
+    }, []);
+    
     return (
         <>
             <img src={Jungle1} alt="Jungle" className="jungle-1" />
